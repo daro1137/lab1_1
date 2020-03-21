18cc145569ad8e7b16a0c6904d2580291265dd55
DariuszJoz <jozwik.darius@gmail.com>
2020-03-21 16:35:20 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/lab1_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_1.dir/main.cpp.o"
+  "lab1_1.pdb"
+  "lab1_1.exe"
+  "liblab1_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
