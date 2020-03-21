87143c5ee32c852056dc3ad59493f5a3802637c7
DariuszJoz <jozwik.darius@gmail.com>
2020-03-21 16:46:57 +0100

    Commited
    Commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main ()
 {
     cout << "Hello World" << endl;
-    cout << "3ci commit" << endl; 
+    cout << "3ci commit" << endl;
+    cout << "Commit" << endl;
     return 0;
 }
